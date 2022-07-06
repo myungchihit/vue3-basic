8695858605f4e0e4a06a76b3bef120deeefc8ae5
JJangGyu <pridom112@naver.com>
2022-07-06 17:24:33 +0900

    vue3-ref

--- learn-vue3/src/App.vue
+++ learn-vue3/src/App.vue
@@ -1,29 +1,22 @@
 <template>
 	<div>
-		<!-- template에서는 message를 제외한다. -->
-		<p>{{ message }}</p>
-		<button v-on:click="addMessage">add click</button>
+		<p>author : {{ autho }}</p>
+		<p>author : {{ autho }}</p>
 	</div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { reactive } from 'vue';
 
 export default {
 	setup() {
-		// reactive는 reference type의 반응형
-		//let message = reactive('Hello Vue!');
+		const boox = reactive({
+			author: 'Vue team',
+			year: '2020',
+			title: 'v3 Guide',
+		});
 
-		// ref는 primitive type의 반응형
-		let message = ref('Hello Vue!');
-
-		const addMessage = () => {
-			message.value = message.value + '!';
-		};
-		return {
-			message,
-			addMessage,
-		};
+		return {};
 	},
 };
 </script>
--- lesson-code/chapter4/App_ref.vue
+++ lesson-code/chapter4/App_ref.vue
@@ -20,6 +20,7 @@ export default {
 		const addMessage = () => {
 			message.value = message.value + '!';
 		};
+
 		return {
 			message,
 			addMessage,
