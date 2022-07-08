b8135e6c7db9cba1ff851cb7568f04b1490bc1b0
JJangGyu <pridom112@naver.com>
2022-07-08 21:35:23 +0900

    class and style binding

---
+++ lesson-code/chapter4/App_class_binding.vue
@@ -0,0 +1,56 @@
+<template>
+	<div>
+		<!-- <div class="text" :class="{ active: isActive, 'text-danger': hasError }"> -->
+		<div
+			class="text"
+			:class="[isActive ? 'active-class' : 'class', errorClass, classObject]"
+		>
+			텍스트!
+		</div>
+		<button v-on:click="toggle">toggle</button>
+		<button v-on:click="hasError = !hasError">toggleError</button>
+	</div>
+</template>
+
+<script>
+import { reactive, ref, computed } from 'vue';
+
+export default {
+	setup() {
+		const isActive = ref(true);
+		const hasError = ref(false);
+
+		// const classObject = reactive({
+		// 	active: true,
+		// 	'text-danger': true,
+		// });
+
+		const classObject = computed(() => {
+			return {
+				active: true && true,
+				'text-danger': true && true,
+				'text-blue': true,
+			};
+		});
+
+		const activeClass = ref('active');
+		const errorClass = ref('error');
+
+		const toggle = () => {
+			isActive.value = !isActive.value;
+		};
+
+		return { isActive, toggle, hasError, classObject, activeClass, errorClass };
+	},
+};
+</script>
+
+<style scoped>
+.active {
+	font-weight: 900;
+}
+
+.text-danger {
+	color: red;
+}
+</style>
---
+++ lesson-code/chapter4/App_style_binding.vue
@@ -0,0 +1,38 @@
+<template>
+	<div>
+		<div :style="styleObject">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate sit
+			quaerat rerum? Neque quia repudiandae officiis officia eos fugiat
+			praesentium molestiae voluptatibus ea, deserunt enim dolorum ratione sequi
+			sint eius.
+		</div>
+		<button v-on:click="fontSize--">-</button>
+		<button v-on:click="fontSize++">+</button>
+	</div>
+</template>
+
+<script>
+import { computed, ref } from 'vue';
+import { reactive } from 'vue';
+
+export default {
+	setup() {
+		// const styleObject = reactive({
+		// 	color: 'red',
+		// 	fontSize: '13px',
+		// });
+
+		const fontSize = ref(13);
+
+		const styleObject = computed(() => {
+			return {
+				color: 'red',
+				fontSize: fontSize.value + 'px',
+			};
+		});
+		return { styleObject, fontSize };
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
