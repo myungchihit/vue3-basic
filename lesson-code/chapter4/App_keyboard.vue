d1c1d15883a479eb8b22a820d9d444d16534bee0
JJangGyu <pridom112@naver.com>
2022-09-25 22:28:03 +0900

    app_keyboard rollback

--- lesson-code/chapter4/App_keyboard.vue
+++ lesson-code/chapter4/App_keyboard.vue
@@ -1,62 +1,31 @@
 <template>
 	<div>
-		<div id="modifiers">
-			<div @click="clickDiv">
-				DIV 영역
-				<p @click.self="clickP">
-					P 영역
-					<!-- <span @click.stop="clickSpan"> SPAN 영역 </span> -->
-					<span @click="clickSpan"> SPAN 영역 </span>
-					<a href="https://naver.com" @click.prevent.stop="clickA">a 영역</a>
-				</p>
-			</div>
-		</div>
-		<button @click.once="clickDiv">once</button>
+		<!-- keyup.enter: enter가 입력되었을 경우 -->
+		<!-- keyup.ctrl.enter : ctrl + enter를 입력해야 이벤트 실행 -->
+		<!-- alt등 다양하게 있음 -->
+		<input type="text" @keyup.ctrl.enter="addTodo" />
+		<ul>
+			<li v-for="(todo, index) in todos" :key="index">
+				{{ todo }}
+			</li>
+		</ul>
 	</div>
 </template>
 
 <script>
+import { reactive } from 'vue';
 export default {
 	setup() {
-		const clickDiv = () => {
-			console.log('clickDiv');
-			// location.href = 'https://naver.com';
+		const todos = reactive([]);
+
+		const addTodo = event => {
+			todos.push(event.target.value);
+			event.target.value = '';
+			event.target.focus();
 		};
-		const clickP = () => {
-			console.log('clickP');
-		};
-		const clickSpan = () => {
-			console.log('clickSpan');
-			// alert('좋아요');
-		};
-		const clickA = e => {
-			// e.preventDefault();
-			alert('어떤기능~');
-		};
-		return {
-			clickDiv,
-			clickP,
-			clickSpan,
-			clickA,
-		};
+		return { todos, addTodo };
 	},
 };
 </script>
 
-<style scoped>
-#modifiers div,
-#modifiers p,
-#modifiers span {
-	padding: 40px;
-}
-#modifiers div {
-	background-color: #ccc;
-}
-#modifiers p {
-	background-color: #999;
-}
-#modifiers span {
-	background-color: #666;
-	display: block;
-}
-</style>
+<style lang="scss" scoped></style>
