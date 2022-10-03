9d0f89094f30427981804984266b6e2b0f515018
JJangGyu <pridom112@naver.com>
2022-10-04 00:02:05 +0900

    components provide/inject

--- learn-vue3/src/components/ProvideInject.vue
+++ learn-vue3/src/components/ProvideInject.vue
@@ -3,6 +3,8 @@
 		<div class="card">
 			<div class="card-header">ProvideInject Component</div>
 			<div class="card-body">
+				<button @click="count++">Click</button>
+				<p>appMessage: {{ appMessage }}</p>
 				<ProvideChilde></ProvideChilde>
 			</div>
 		</div>
@@ -10,7 +12,7 @@
 </template>
 
 <script>
-import { provide, ref } from 'vue';
+import { inject, provide, ref } from 'vue';
 import ProvideChilde from './ProvideChilde.vue';
 export default {
 	components: {
@@ -19,12 +21,20 @@ export default {
 	setup() {
 		const staticMessage = 'static message';
 		const message = ref('message');
+		const updateMessage = appendMessage => {
+			message.value = message.value + appendMessage;
+		};
 		const count = ref(10);
 		provide('static-message', staticMessage);
-		provide('message', message);
+		provide('message', { message, updateMessage });
 		provide('count', count);
 
-		return {};
+		const appMessage = inject('app-message');
+
+		return {
+			count,
+			appMessage,
+		};
 	},
 };
 </script>
--- learn-vue3/src/main.js
+++ learn-vue3/src/main.js
@@ -3,12 +3,14 @@ import { createApp } from 'vue';
 import App from './App.vue';
 //import AppCard from '@/components/AppCard.vue';
 
-createApp(App).mount('#app');
+// createApp(App).mount('#app');
 
 // 전역 컴포넌트 등록방법.
-//const app = createApp(App);
+const app = createApp(App);
 //app.component('AppCard', AppCard);
-//app.mount('#app');
+
+app.provide('app-message', 'app Message 입니다.');
+app.mount('#app');
 
 // mount 먼저시키고 import 시켜야함
 import 'bootstrap/dist/js/bootstrap.js';
